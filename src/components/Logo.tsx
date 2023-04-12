5bea08be6e8182cffbafad2b4fc2f7644602e3a3
Murat Istek <messi897@gmail.com>
2023-04-12 19:30:41 -0400

    Add initial logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+import { motion } from 'framer-motion'
+
+// Framer Motion vars
+const MotionLink = motion(Link)
+
+export default function Logo() {
+  return (
+    <div className='flex justify-center items-center mt-2'>
+      <MotionLink
+        href='/'
+        className='h-16 w-16 text-light bg-dark flex justify-center items-center rounded-full text-2xl font-bold'
+        whileHover={{
+          scale: 1.5,
+          backgroundColor: ["#121212", "rgba(131,58,180,1)", "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)", "#121212"],
+          transition: { duration: 1, repeat: Infinity }
+        }}
+      >MI</MotionLink>
+    </div>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,38 @@
 import Link from 'next/link'
 import React from 'react'
+import Logo from './Logo'
+import { NextRouter, useRouter } from 'next/router'
+
+interface CustomLinkProps {
+  href: string
+  title: string
+  className?: string
+}
+
+const CustomLink = ({ href, title, className = "" }: CustomLinkProps) => {
+  const router: NextRouter = useRouter()
+  const currentPath: string = router.asPath
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span className={`absolute h-[1px] ${currentPath === href ? "w-full" : "w-0"} inline-block bg-dark -bottom-0.5 left-0 group-hover:w-full transition-[width] ease duration-300`}>&nbsp;</span>
+    </Link>
+  )
+}
+
 
 export default function Navbar() {
   return (
     <header className='font-medium w-full px-32 py-8 flex items-center justify-between'>
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/articles">Articles</Link>
+        <CustomLink href="/" title='Home' className='mr-4' />
+        <CustomLink href="/about" title='About' className='mx-4' />
+        <CustomLink href="/projects" title='Projects' className='mx-4' />
+        <CustomLink href="/articles" title='Articles' className='ml-4' />
       </nav>
-      <h2>Logo</h2>
+
       <nav>
         <Link href='/' target={'_blank'}>T</Link>
         <Link href='/' target={'_blank'}>T</Link>
@@ -19,6 +41,10 @@ export default function Navbar() {
         <Link href='/' target={'_blank'}>T</Link>
         <Link href='/' target={'_blank'}>T</Link>
       </nav>
+
+      <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo />
+      </div>
     </header>
   )
 }
